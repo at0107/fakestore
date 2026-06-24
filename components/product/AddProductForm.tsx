00ca1d821b805fa0e46ef5328b915ea6ef898f0b
at0107 <aliktadevosyan04@gmail.com>
2026-06-24 17:56:20 +0400

    fourth commit

--- components/product/AddProductForm.tsx
+++ components/product/AddProductForm.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react"
 import Link from "next/link"
 import type { Product } from "@/types/product"
+import { BASE_URL } from "@/lib/constants"
 
 const EMPTY_PRODUCT: Product = {
     id: 0,
@@ -24,34 +25,101 @@ const CATEGORIES = ["men's clothing",
 
 
 export default function AddProductForm() {
-    const [product, setProduct] = useState<Product>(EMPTY_PRODUCT);
+    const [fields, setFields] = useState<Product>(EMPTY_PRODUCT);
     const [errors, setErrors] = useState<string | null>(null);
     const [isLoading, setIsLoading] = useState(false);
     const [createdProduct, setCreatedProduct] = useState<Product | null>(null);
 
-    const handleChange = () => {};
-    const handleSubmit = () => {};
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const {name,value} = event.target;
+        setFields({...fields,[name]: value})
+    };
+
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        setErrors(null);
+        setIsLoading(true);
+        try{
+            const response = await fetch(`${BASE_URL}/products`, {
+                method: 'POST',
+                body: JSON.stringify(fields),
+            })
+            if(!response.ok){
+                throw new Error("Failed to create product")
+            }
+            const data = await response.json();
+            setCreatedProduct(data)
+        } catch (error:unknown) {
+            setErrors(error instanceof Error ? error.message: "An unknown error occured")
+        } finally {
+            setIsLoading(false)
+        }
+    };
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title" name="title" />
+                <input 
+                type="text" 
+                id="title" 
+                name="title" 
+                value={fields.title}
+                onChange={handleChange}
+                required
+                />
             </div>
             <div>
                 <label htmlFor="price">Price</label>
-                <input type="number" id="price" name="price" />
+                <input 
+                type="number" 
+                id="price" 
+                name="price" 
+                value={fields.price}
+                onChange={handleChange}
+                required
+                />
             </div>
              <div>
                 <label htmlFor="description">Description</label>
-                <textarea name="description" id="description"></textarea>
+                <textarea 
+                name="description" 
+                id="description"
+                value={fields.description}
+                onChange={handleChange}
+                required
+                />
             </div>
             <div>
                 <label htmlFor="category">Category</label>
-                <select name="category" id="category">
+                <select 
+                name="category" 
+                id="category"
+                value={fields.category}
+                onChange={handleChange}
+                required
+                >
                     <option value="">Select a category</option>
+                    {CATEGORIES.map((category) => (
+                        <option key={category} value="{category}">{category}</option>
+                    ))}
                 </select>
             </div>
+            <div>
+                <label htmlFor="image">Image *</label>
+                <input 
+                type="text"
+                id="image"
+                name="image"
+                value={fields.image}
+                onChange={handleChange}
+                required
+                 />
+            </div>
+
+            <button type="submit" disabled={isLoading}>
+                {isLoading ? "Creating": "Create Product"}
+            </button>
         </form>
     )
 }
